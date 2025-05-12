d3ee7d80136c37c74c8b501967aff6fff0ebd7c9
eutawan <principalacc169@gmail.com>
2025-05-12 19:32:20 -0300

    Obtendo um usuário por id (exception não funciona)

--- src/main/java/com/course/workshopmongo/resources/UserResource.java
+++ src/main/java/com/course/workshopmongo/resources/UserResource.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
@@ -28,7 +29,13 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
 
+    @Operation(summary = "buscar usuário por ID", description = "retorna apenas um usuário encontrado pelo ID")
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 
 }
--- src/main/java/com/course/workshopmongo/services/UserService.java
+++ src/main/java/com/course/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.course.workshopmongo.services;
 
 import com.course.workshopmongo.domain.User;
 import com.course.workshopmongo.repository.UserRepository;
+import com.course.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+
     }
 
 }
